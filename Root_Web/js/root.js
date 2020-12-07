8c960cbbdfe9eb405916a0a870c5c22caf312bd9
Ch10316 <andrewlime04@naver.com>
2020-12-07 20:18:59 +0900

    second

--- Root_Web/js/root.js
+++ Root_Web/js/root.js
@@ -8,20 +8,18 @@ $(document).on('mouseenter mouseleave', '.menu>ul>li', menu)
 
 // 복사 방지, 드래그 방지
 
-function select(e){
-    var tags = ["input", "textarea", "select"]
-    if (tags.indexOf(e.target.tagName.toLowerCase()) == -1){
-        return false
-    }
-}
-function reEnable() {
-    return true
-}
-if (typeof document.onselectstart != "undefined"){
-    document.onselectstart = new Function ("return false")
-}else{
-    document.onmousedown = select
-    document.onmouseup = reEnable
-}
-
-// 
+// function select(e){
+//     var tags = ["input", "textarea", "select"]
+//     if (tags.indexOf(e.target.tagName.toLowerCase()) == -1){
+//         return false
+//     }
+// }
+// function reEnable() {
+//     return true
+// }
+// if (typeof document.onselectstart != "undefined"){
+//     document.onselectstart = new Function ("return false")
+// }else{
+//     document.onmousedown = select
+//     document.onmouseup = reEnable
+// }
