a113659eba5d5f05efacaf64e88e8cd0aea5de97
BackHoe312 <61866551+BackHoe312@users.noreply.github.com>
2021-09-22 19:30:03 +0900

    Update db.php

--- Root_Web/db.php
+++ Root_Web/db.php
@@ -3,7 +3,7 @@
     header('Content-Type: text/html; charset=utf-8'); // utf-8 encoding
 
     // localhost = DB 주소, 계정아이디, DB비밀번호, DB이름
-    $db = new mysqli("localhost", "rootWeb", "jynhub2112", "root_board");
+    $db = new mysqli("localhost", "rootWeb", "my-password", "root_board");
     $db->set_charset("utf8");
 
     function mq($sql) {
